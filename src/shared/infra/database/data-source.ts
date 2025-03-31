2d56e26da1f3bb779e58be6ae03377c012cfc2ec
Adrian Campana <adrianphcampana12@gmail.com>
2025-03-31 10:56:33 -0300

    feat: configure database host using environment variable

--- src/shared/infra/database/data-source.ts
+++ src/shared/infra/database/data-source.ts
@@ -4,6 +4,7 @@ import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
+  host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
